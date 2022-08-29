605463cae577f57e626980609c4f049982882987
muyaho <97763414+moayo@users.noreply.github.com>
2022-08-30 06:55:22 +0900

    Update 125. Valid Palindrome_Slicing.py

--- 125. Valid Palindrome_Slicing.py
+++ 125. Valid Palindrome_Slicing.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        s = re.sub('[^a-z0-9]', '', s)
+        s = re.sub('[^a-z0-9]', '', s) # re.sub(pattern, replace, text)
 
         return s == s[::-1]
